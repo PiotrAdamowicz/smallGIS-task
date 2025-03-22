e111fc87e8770504beb29d6b0482bf282ef519a0
Piotr Adamowicz <adamowicz.piotr2@gmail.com>
2025-03-22 13:59:57 +0100

    feat: :wastebasket: code cleanup

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,3 @@
-<script setup lang="ts">
-import HelloWorld from "./components/HelloWorld.vue";
-</script>
+<script setup lang="ts"></script>
 
-<template>
-  <HelloWorld msg="Vite + Vue" />
-</template>
+<template>Clean Setup</template>
