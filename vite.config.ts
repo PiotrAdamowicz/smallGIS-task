ff514693bbb8a7d3eb717903dba3b269cdf23d72
Piotr Adamowicz <adamowicz.piotr2@gmail.com>
2025-03-22 12:36:39 +0100

    feat: :wrench: setup testing

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,12 @@
-import { defineConfig } from "vite";
+import { defineConfig } from "vitest/config";
 import tailwindcss from "@tailwindcss/vite";
 import vue from "@vitejs/plugin-vue";
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [vue(), tailwindcss()]
+  plugins: [vue(), tailwindcss()],
+  test: {
+    globals: true,
+    environment: "happy-dom"
+  }
 });
