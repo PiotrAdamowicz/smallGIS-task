982680c5dd21ad83901d4a06d47d3718344bbda7
Piotr Adamowicz <adamowicz.piotr2@gmail.com>
2025-03-24 11:50:56 +0100

    feat: :zap: adjusted main layout - footer at the bottom

--- src/App.vue
+++ src/App.vue
@@ -6,5 +6,5 @@ import TaskList from "./components/Tasks/TaskList.vue";
 <template>
   <TaskList />
   <AddTask />
-  <footer class="px-4 pb-4 fixed bottom-0 left-0 right-0">Made by me</footer>
+  <footer class="px-4 pb-4 bottom-0 left-0 right-0 mr-14">Made by me</footer>
 </template>
