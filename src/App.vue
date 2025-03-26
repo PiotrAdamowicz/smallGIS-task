2498f28a6bdb71b2840014b1f99ef2dd53fa0e50
Piotr Adamowicz <adamowicz.piotr2@gmail.com>
2025-03-26 21:34:06 +0100

    chore: :truck: adjustet component name and paths

--- src/App.vue
+++ src/App.vue
@@ -1,14 +1,14 @@
 <script setup lang="ts">
 import { ref } from "vue";
 import AddTask from "./components/AddTask/AddTask.vue";
-import Navigation from "./components/Navigation/Navigation.vue";
+import Header from "./components/Header/Header.vue";
 import TaskList from "./components/Tasks/TaskList.vue";
 
 const sortAscending = ref<boolean>(false);
 </script>
 
 <template>
-  <Navigation v-model:sortAscending="sortAscending" />
+  <Header v-model:sortAscending="sortAscending" />
   <TaskList :sortAscending="sortAscending" />
   <AddTask />
   <footer class="px-4 pb-4 bottom-0 left-0 right-0 mr-14">Made by me</footer>
--- src/components/Header/Header.vue
+++ src/components/Header/Header.vue
@@ -6,12 +6,12 @@ import DarkModeToggle from "./DarkModeToggle.vue";
 </script>
 
 <template>
-  <nav class="mx-4 mb-2 mt-4">
+  <header class="mx-4 mb-2 mt-4">
     <Search />
     <div class="w-full flex flex-col sm:flex-row gap-x-3 gap-y-2">
       <SortByDate />
       <FilterByStatus />
       <DarkModeToggle />
     </div>
-  </nav>
+  </header>
 </template>
