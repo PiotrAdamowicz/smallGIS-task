6015e4f213aa8132adb9759191d180b6c31a4895
Piotr Adamowicz <adamowicz.piotr2@gmail.com>
2025-03-26 12:36:27 +0100

    test: :white_check_mark: added store tests

--- src/utils/data.ts
+++ src/utils/data.ts
@@ -1,24 +1,73 @@
+//tasksData used in tests and for store init data for Demo App
 export const tasksData = [
   {
     id: "1",
     title: "Task 1",
-    created: new Date("2022-03-01"),
-    description: "This is task 1",
-    completed: false
-  },
-  {
-    id: "3",
-    title: "Task 3",
-    created: new Date(),
-    description:
-      "This is task 3 This is task 3 This is task 3 This is task 3 This is task 3 This is task 3 This is task 3 This is task 3 This is task 3 This is task 3 This is task 3 This is task 3 ",
+    created: new Date("2020-01-01"),
+    description: "Red",
     completed: false
   },
   {
     id: "2",
     title: "Task 2",
-    created: new Date("2023-09-01"),
-    description: "This is task 2",
+    created: new Date("2021-01-01"),
+    description: "Blue",
     completed: true
+  },
+  {
+    id: "3",
+    title: "Task 3",
+    created: new Date("2022-01-01"),
+    description: "Yellow or green?",
+    completed: false
+  },
+  {
+    id: "4",
+    title: "Task 4",
+    created: new Date("2023-01-01"),
+    description: "Purple",
+    completed: false
+  },
+  {
+    id: "5",
+    title: "Task 5",
+    created: new Date("2024-01-01"),
+    description: "White",
+    completed: false
+  },
+  {
+    id: "6",
+    title: "Task 6",
+    created: new Date("2025-01-01"),
+    description: "White brown",
+    completed: false
+  },
+  {
+    id: "7",
+    title: "Task 7",
+    created: new Date("2025-02-01"),
+    description: "Orange",
+    completed: false
+  },
+  {
+    id: "8",
+    title: "Task 8",
+    created: new Date("2025-03-01"),
+    description: "Black",
+    completed: true
+  },
+  {
+    id: "9",
+    title: "Task 9",
+    created: new Date("2025-03-02"),
+    description: "Kinda blue",
+    completed: false
+  },
+  {
+    id: "10",
+    title: "Task 10",
+    created: new Date("2025-03-03"),
+    description: "Pink",
+    completed: false
   }
 ];
--- tests/useTasksStore.spec.ts
+++ tests/useTasksStore.spec.ts
@@ -1,12 +1,15 @@
 import { setActivePinia, createPinia } from "pinia";
 import { useTasksStore } from "../src/store/useTasksStore";
 import { describe, beforeEach, it, expect } from "vitest";
+import { uid } from "../src/utils/uid.ts";
+import { searchByQuery } from "../src/helpers/searchByQuery";
+import { sortByDate } from "../src/helpers/sortTasks";
 
 const newTask = {
-  id: "4",
-  title: "Task 4",
+  id: uid(),
+  title: "Task " + Math.random(),
   created: new Date(),
-  description: "This is task 4",
+  description: "This is test task",
   completed: false
 };
 
@@ -15,18 +18,77 @@ describe("Use Tasks Store", () => {
     setActivePinia(createPinia());
   });
 
-  it("Adds task", () => {
+  it("Begin with 10 tasks", () => {
     const store = useTasksStore();
-    expect(store.tasks).toHaveLength(3);
-    store.addTask(newTask);
-    expect(store.tasks).toHaveLength(4);
+    expect(store.tasks).toHaveLength(10);
   });
-  //TODO: chek for removing specific task
-  //Add initial state in test
-  it("Removes task", () => {
+
+  it("Toggle sorting state", () => {
     const store = useTasksStore();
-    expect(store.tasks).toHaveLength(3);
-    store.removeTask("1");
-    expect(store.tasks).toHaveLength(2);
+    const CURRENT = store.sortAscending;
+    const EXPECTED = !store.sortAscending;
+
+    expect(store.sortAscending).toBe(CURRENT);
+    store.toggleSort();
+    expect(store.sortAscending).toBe(EXPECTED);
+  });
+
+  it("Search by query", () => {
+    const store = useTasksStore();
+    const TASKS = store.tasks;
+    const QUERY = "Blue";
+
+    expect(searchByQuery(TASKS, QUERY)).toHaveLength(2);
+    expect(searchByQuery(TASKS, "")).toHaveLength(10);
+    expect(searchByQuery(TASKS, "random query adnasidnasldi")).toHaveLength(0);
+  });
+
+  it("Sort tasks by date ascending", () => {
+    const store = useTasksStore();
+    const TASKS = store.tasks;
+    const SORTED_TASK_DATE = sortByDate(TASKS, true)[0].created;
+
+    expect(SORTED_TASK_DATE).toEqual(new Date("2020-01-01"));
+  });
+
+  it("Sort tasks by date descending", () => {
+    const store = useTasksStore();
+    const TASKS = store.tasks;
+    const SORTED_TASK_DATE = sortByDate(TASKS, false)[0].created;
+
+    expect(SORTED_TASK_DATE).toEqual(new Date("2025-03-03"));
+  });
+
+  it("Filter tasks by 'Not Done' status", () => {
+    const store = useTasksStore();
+
+    const FILTERED_TASKS = store.getFilteredTasks(true);
+    expect(FILTERED_TASKS).toHaveLength(8);
+  });
+
+  it("Filter tasks by 'Done' status", () => {
+    const store = useTasksStore();
+
+    const FILTERED_TASKS = store.getFilteredTasks(false);
+    expect(FILTERED_TASKS).toHaveLength(10);
+  });
+  it("Add task", () => {
+    const store = useTasksStore();
+    const LENGTH = store.tasks.length;
+    const EXPECTED = store.tasks.length + 1;
+
+    expect(store.tasks).toHaveLength(LENGTH);
+    store.addTask(newTask);
+    expect(store.tasks).toHaveLength(EXPECTED);
+  });
+
+  it("Remove task", () => {
+    const store = useTasksStore();
+    const LENGTH = store.tasks.length;
+    const EXPECTED = store.tasks.length - 1;
+
+    expect(store.tasks).toHaveLength(LENGTH);
+    store.removeTask("10");
+    expect(store.tasks).toHaveLength(EXPECTED);
   });
 });
